9734cf27fc20acecc27bd84bb107de01ddf325b3
yesmineRz <yesmine.rezgui@gmail.com>
2016-11-10 13:58:03 +0100

    bugfix

--- src/main/java/io/oilfox/backend/api/controllers/ChartController.java
+++ src/main/java/io/oilfox/backend/api/controllers/ChartController.java
@@ -489,7 +489,7 @@ public class ChartController extends AbstractController{
     public Response getReminder(@PathParam("id") String id, @PathParam("delay") String delay, @PathParam("type") String type) throws Exception {
 
         if(delay.equals("1")){
-            TimeUnit.SECONDS.sleep(30);
+            TimeUnit.SECONDS.sleep(10);
 
             Response r = null;
 
